f89150620c5fa89cac05c68831f3a25ec3777375
shafeen <sodl02@gmail.com>
2015-11-21 21:58:28 -0500

    updated PostModel.php

--- framework/models/PostModel.php
+++ framework/models/PostModel.php
@@ -67,7 +67,7 @@ class PostModel
                     <a href="#">
                         <img class="img-responsive" src="/images/post-sample-image.jpg" alt="">
                     </a>
-                    <span class="caption text-muted">To go places and do things that have never been done before – that’s what living is all about.</span>
+                    <span class="caption text-muted">To go places and do things that have never been done before -- that\'s what living is all about.</span>
                     <p>Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.</p>
                     <p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there\'s a fundamental truth to our nature, Man must explore, and this is exploration at its greatest.</p>
                     <p>Placeholder text by <a href="http://spaceipsum.com/">Space Ipsum</a>. Photographs by <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>.</p>
