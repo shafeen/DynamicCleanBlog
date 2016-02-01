e835ae7cd80da73cb8ebd4951660188432cb1ce5
Shafeen M <sodl02@gmail.com>
2016-01-31 23:50:25 -0500

    Updated TaggedController with sql query to grab tagged posts This query will have to be updated so that multiple tagnames can be checked at once.

--- framework/controllers/TaggedController.php
+++ framework/controllers/TaggedController.php
@@ -11,6 +11,34 @@ require_once("models/TaggedModel.php");
 
 class TaggedController extends ModuleController
 {
+    // TODO: move database access to a separate component
+    private function getTaggedPostsFromDb($tagname) {
+        if (empty($tagname)) {
+            return null;
+        }
+
+        global $DB_INFO;
+        $dbConn = mysqli_connect(
+            $DB_INFO["db_addr"],
+            $DB_INFO["username"],
+            $DB_INFO["password"],
+            $DB_INFO["main_db_name"]);
+
+        // clean the tagname -> todo: use parameterized query$
+        $tagname = $dbConn->real_escape_string($tagname);
+        $sql = "SELECT
+                  posts.id as post_id,
+                  posts.created,
+                  posts.title as post_title,
+                  tags.tagname FROM posts
+                  JOIN tagged_posts ON posts.id = tagged_posts.post_id
+                  JOIN tags ON tagged_posts.tag_id = tags.id
+                WHERE tagname='$tagname'";
+        $result = $dbConn->query($sql);
+        $taggedPostObjs = $result->fetch_all(); // TODO: debug this
+        return $taggedPostObjs;
+    }
+
     /** @return TaggedModel*/
     function initTaggedModel() {
         // TODO: initialize the tagged model here
@@ -20,7 +48,7 @@ class TaggedController extends ModuleController
         // The About module view page does not have need to be dynamic.
         $this->moduleModel = null; // TODO: call to initModel here
 //        $this->moduleView = new TaggedView($this->moduleModel); // TODO: create view
-//        $this->moduleView->setMainHtmlFile("tagged.phtml"); // TODO: create main-html
+//        $this->moduleView->setMainHtmlFile("tagged.phtml"); // TODO: create main-html (for non rest api page)
 //        $this->moduleView->displayContent();
     }
 
