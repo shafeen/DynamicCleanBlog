3bc40479240a4908a061d1971c2ce47245294845
Shafeen M <sodl02@gmail.com>
2016-02-15 23:54:24 -0500

    Updated TaggedController - Added view files the View class and the html view file

---
+++ framework/views/TaggedView.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace framework\views;
+
+use framework\models\TaggedModel;
+
+class TaggedView extends ModuleView
+{
+    protected function initStaticInfo() {
+        // no static data
+    }
+
+    /** @param TaggedModel $model */
+    protected function extractInfoFromModel(&$model) {
+        // TODO: Implement extractInfoFromModel() method.
+    }
+
+}
