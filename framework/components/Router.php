e5a0f22e88e96fb0e0c45c712d6d16077a8f2674
Shafeen M <sodl02@gmail.com>
2016-03-10 00:25:35 -0500

    updated Router class to be leaner

--- framework/components/Router.php
+++ framework/components/Router.php
@@ -15,42 +15,26 @@ use framework\controllers\NotFoundController;
 // TODO: refactor this VERY basic router
 class Router extends Singleton
 {
+    // when adding a new module route -->
+    // add the module name to Controller here in this array
     private $moduleToControllerMap = array(
-        'home'    => 'HomeController.php',
-        'post'    => 'PostController.php',
-        'about'   => 'AboutController.php',
-        'contact' => 'ContactController.php',
-        'tagged'  => 'TaggedController.php',
-        'notfound'=> 'NotFoundController.php'
+        'home'    => 'HomeController',
+        'post'    => 'PostController',
+        'about'   => 'AboutController',
+        'contact' => 'ContactController',
+        'tagged'  => 'TaggedController',
+        'notfound'=> 'NotFoundController'
     );
 
     public function routeToModuleController() {
         $explodedCleanRequestUrl = CleanRequestUrlParser::instance()->getExplodedCleanRequestUrl();
         $moduleToRun = $this->getModuleNameFromRequestUrl($explodedCleanRequestUrl);
 
-        // intialize the correct module's controller
-        $moduleController = null;
-        if ($moduleToRun==='post') {
-            require_once("controllers/PostController.php");
-            $moduleController = new PostController(array_slice($explodedCleanRequestUrl, 0, 1));
-        } else if (empty($moduleToRun) || $moduleToRun==='home') {
-            require_once("controllers/HomeController.php");
-            $moduleController = new HomeController(array_slice($explodedCleanRequestUrl, 0, 1));
-        } else if ($moduleToRun==='about') {
-            require_once("controllers/AboutController.php");
-            $moduleController = new AboutController(array_slice($explodedCleanRequestUrl, 0, 1));
-        } else if ($moduleToRun==='contact') {
-            require_once("controllers/ContactController.php");
-            $moduleController = new ContactController(array_slice($explodedCleanRequestUrl, 0, 1));
-        } else if ($moduleToRun==='tagged') {
-            // TODO: complete the implementation of the TaggedController
-            require_once("controllers/TaggedController.php");
-            $moduleController = new TaggedController(array_slice($explodedCleanRequestUrl, 0, 1));
-        } else {
-            require_once("controllers/NotFoundController.php");
-            $moduleController = new NotFoundController(array_slice($explodedCleanRequestUrl, 0, 1));
-        }
-        return $moduleController;
+        // return the correct intialized module's controller
+        $controllerName = $this->moduleToControllerMap[$moduleToRun];
+        require_once('controllers/'.$controllerName.'.php');
+        $fullyQualifiedControllerName = 'framework\\controllers\\'.$controllerName;
+        return new $fullyQualifiedControllerName(array_slice($explodedCleanRequestUrl, 0, 1));
     }
 
     private function getModuleNameFromRequestUrl($explodedCleanRequestUrl) {
