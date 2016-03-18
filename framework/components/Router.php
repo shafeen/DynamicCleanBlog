5f6ba29bcf1ef82510d29363e18e11362f154fbd
shafeen <sodl02@gmail.com>
2016-03-17 23:36:47 -0400

    updated Router component class to verify url formats for modules

--- framework/components/Router.php
+++ framework/components/Router.php
@@ -18,21 +18,29 @@ class Router extends Singleton
     // when adding a new module route -->
     // add the module name to Controller here in this array
     // TODO: need a way to verify expected url format for a controller in the Router
+    // modulename => array(ModuleControllerName, RegexForURL)
     private $moduleToControllerMap = array(
-        'home'    => 'HomeController',
-        'post'    => 'PostController',
-        'about'   => 'AboutController',
-        'contact' => 'ContactController',
-        'tagged'  => 'TaggedController',
-        'notfound'=> 'NotFoundController'
+        'home'    => array('HomeController',     '/(^\/?home\/page\/[0-9]+\/?$)|(^\/?home\/?$)|(^\/?$)/'),
+        'post'    => array('PostController',     '/^\/?post\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/(.*)/'),
+        'about'   => array('AboutController',    '/^\/?about\/?$/'),
+        'contact' => array('ContactController',  '/^\/?contact\/?$/'),
+        'tagged'  => array('TaggedController',   '/^\/?tagged\/tags\/[a-zA-Z]+(:[a-zA-Z]+)*(\/|(\/page\/[0-9]+\/?))?$/'),
+        'notfound'=> array('NotFoundController', '(.*)') // no regex match needed or used
     );
 
     public function routeToModuleController() {
         $explodedCleanRequestUrl = CleanRequestUrlParser::instance()->getExplodedCleanRequestUrl();
+        $cleanRequestUrl = CleanRequestUrlParser::instance()->getCleanRequestUrl();
         $moduleToRun = $this->getModuleNameFromRequestUrl($explodedCleanRequestUrl);
 
         // return the correct intialized module's controller
-        $controllerName = $this->moduleToControllerMap[$moduleToRun];
+        $controllerUrlRegex = $this->moduleToControllerMap[$moduleToRun][1];
+        $controllerName = $this->moduleToControllerMap[$moduleToRun][0];
+        $defaultControllerName = $this->moduleToControllerMap['notfound'][0];
+        if (!preg_match($controllerUrlRegex, $cleanRequestUrl)) {
+            $controllerName = $defaultControllerName;
+        }
+
         require_once('controllers/'.$controllerName.'.php');
         $fullyQualifiedControllerName = 'framework\\controllers\\'.$controllerName;
         return new $fullyQualifiedControllerName(array_slice($explodedCleanRequestUrl, 0, 1));
